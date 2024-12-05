85bab91c4d035951dd65df2feffc04f5ef7408e4
Becky Williams <beckz14@gmail.com>
2024-12-05 12:36:09 +0000

    post model changed & added ingredient model

--- blog/models.py
+++ blog/models.py
@@ -6,18 +6,23 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
 
-
 class Post(models.Model):
-    """
-    Stores a single blog post entry related to :model:`auth.User`.
-    """
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
     featured_image = CloudinaryField('image', default='placeholder')
-    content = models.TextField()
+    servings = models.IntegerField(null=True)
+    timings = models.IntegerField(null=True)
+    content = models.TextField(null=True)
+    category_choices = [
+        ('GF', 'Gluten Free'),
+        ('QM', '20mins or less'),
+        ('Vg', 'Vegan'),
+        ('V', 'Vegetarian')
+    ]
+    category = models.CharField(max_length=2, choices=category_choices, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
@@ -29,12 +34,29 @@ class Post(models.Model):
     def __str__(self):
         return f"{self.title} | written by {self.author}"
 
+class Ingredient(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="ingredient")
+    name = models.CharField(max_length=150)
+    quantity = models.DecimalField(max_digits=6, decimal_places=2)
+
+    unit_choices = [
+        ("grams","Grams"),
+        ("kilograms","Kilograms"),
+        ("cups","Cups"),
+        ("tablespoon","Tablespoon"),
+        ("teaspoon","Teaspoon"),
+        ("small", "Small"),
+        ("medium", "Medium"),
+        ("large","Large"),
+    ]
+
+    unit = models.CharField(max_length=20, choices=unit_choices, default="grams", blank=True, null=True)    
+
+    def __str__(self):
+        return f"{self.quantity} {self.unit} of {self.name}"
+
 
 class Comment(models.Model):
-    """
-    Stores a single comment entry related to :model:`auth.User`
-    and :model:`blog.Post`.
-    """
     post = models.ForeignKey(Post, on_delete=models.CASCADE,
                              related_name="comments")
     author = models.ForeignKey(
