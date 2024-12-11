413024bb99de22da99ceeb0942a596e537c882c8
Becky Williams <beckz14@gmail.com>
2024-12-11 10:05:26 +0000

    recipe upload page fixed

--- recipe_upload/views.py
+++ recipe_upload/views.py
@@ -9,17 +9,19 @@ def recipe_upload(request):
     if request.method == "POST":
         recipe_form = RecipeUploadForm(request.POST)        
         if recipe_form.is_valid():
-                recipe = recipe_form.save()
+                # recipe = recipe_form.save()
+                recipe = recipe_form.save(commit=False)
                 recipe.author = request.user
-                messages.add_message(request, messages.SUCCESS, "Recipe uploaded successfully!")
+                recipe.save()
+                # messages.add_message(request, messages.SUCCESS, "Recipe uploaded successfully!")
                 messages.success(request, 'Recipe uploaded successfully!')
-        return render(request, 'recipe_upload/upload.html')
+        return render(request, 'recipe_upload/recipe_upload.html')
     else:
         
         recipe_form = RecipeUploadForm()
 
     return render(
         request,
-        'about/about.html',
+        'recipe_upload/recipe_upload.html',
         {'recipe_upload_form': recipe_form}
     )
